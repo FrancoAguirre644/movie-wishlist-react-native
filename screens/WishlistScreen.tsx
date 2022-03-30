36c7bca8cdd689f9974ef3330e7c7707c5365857
FrancoAguirre644 <francoaguirre644@gmail.com>
2022-03-30 20:21:07 -0300

    Project setup.

---
+++ screens/WishlistScreen.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { RootStackScreenProps } from "../types";
+
+export default function WishlistScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Wishlist Screen.</Text>
+            <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
+                <Text style={styles.linkText}>Go to home screen!</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+    linkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+});
